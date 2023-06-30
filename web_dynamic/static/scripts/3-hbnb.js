3b76efac6c86a8ae07a98b552f1d04a4e526771d
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-30 01:31:30 -0700

    Add a post AJAX function to 3-hbnb.js and add all other tracked files to master branch

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready( function() {
+        $("input").on("change") => {
+                wanted = [];
+                if(input.checked) {
+                        wanted.push($("input#id").val());
+                }
+                if(input.checked != true) {
+                        wanted.pop($("input#amenty_id").val());
+                }
+		$("div#amenities h4").val(wanted);
+        }
+	$.get("http://0.0.0.0:5001/api/v1/status/", function(data, tstatus) {
+		if(tstatus === "success") {
+			$("div#api_status").addClass("available");
+		}
+		else {
+			$("div#api_status").removeClass("available");
+		}
+	});
+	$.post("http://0.0.0.0:5001/api/v1/places_search/",
+		'{"Content-type": "application/json"}', function(data. tstatus) {
+			for( key in data) {
+				$("section.places").html("<article>$(key)</article>");
+			}
+		});
+});
